dfebad691f31791fd9bfa1cfb8a68bc9c249cb75
Craig Holzinger <clholzin@gmail.com>
2021-12-06 16:13:16 -0700

    updates

--- server/server.go
+++ server/server.go
@@ -7,15 +7,20 @@ import (
 )
 
 func Start(ip, port string) {
+	swaggerip := "127.0.0.1"
+	swaggerport := "1323"
 	srvdocs := &http.Server{
 		Handler:      routerdocs,
-		Addr:         "127.0.0.1:1323",
+		Addr:         swaggerip + ":" + swaggerport,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 	go func() {
 		log.Println(srvdocs.ListenAndServe())
 	}()
+	log.Printf("Running swagger on %s:%s", swaggerip, swaggerport)
+
+	log.Printf("Running on %s:%s", ip, port)
 
 	srv := &http.Server{
 		Handler:      router,
